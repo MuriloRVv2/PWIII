ce92ed7fc2d5d80712f6b176b0955563d4fdfbea
Murilo Rodrigues <muriloveroneze0987@gmail.com>
2026-08-14 11:57:34 -0300

    Aula inicial de Spring do 3ºBimestre

---
+++ 3Bi/Aula01teste/src/main/java/com/site/aula01teste/Aula01testeApplication.java
@@ -0,0 +1,13 @@
+package com.site.aula01teste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula01testeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aula01testeApplication.class, args);
+    }
+
+}
---
+++ 3Bi/Aula01teste/src/test/java/com/site/aula01teste/Aula01testeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.site.aula01teste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula01testeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
